3e0dab518cba2a3ed000946d28583dbe5112d080
GiuseppeMota <giuseppemota@gmail.com>
2024-05-21 12:11:55 -0300

    dapper configuration

---
+++ src/Cepedi.Banco.Analise.Dados/Queries/PessoaCreditoQueryRepository.cs
@@ -0,0 +1,47 @@
+﻿using Cepedi.Banco.Analise.Dados.Repositorios.Queries;
+using Cepedi.Banco.Analise.Dominio.Entidades;
+using Cepedi.Banco.Analise.Dominio.Repositorio.Queries;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace Cepedi.Banco.Analise.Dados.Queries;
+public class PessoaCreditoQueryRepository : BaseDapperRepository, IPessoaCreditoQueryRepository
+{
+    public PessoaCreditoQueryRepository(IConfiguration configuration) : base(configuration)
+    {
+    }
+
+    public async Task<List<PessoaCreditoEntity>> ObterPessoasAsync(string Cpf)
+    {
+        var parametros = new DynamicParameters();
+        parametros.Add("@Cpf", Cpf, System.Data.DbType.String);
+
+        var query = @"SELECT 
+                        Id, 
+                        Score,
+                        Cpf
+                    FROM PessoaCredito WITH(NOLOCK)
+                    Where
+                        Cpf = @Cpf";
+
+        return (await ExecuteQueryAsync<PessoaCreditoEntity>(query, parametros)).ToList();
+    }
+
+    public async Task<PessoaCreditoEntity?> AtualizarPessoaDapperAsync(PessoaCreditoEntity pessoa)
+    {
+        var parametros = new DynamicParameters();
+        parametros.Add("@LimiteCredito", pessoa.LimiteCredito, System.Data.DbType.String);
+        parametros.Add("@Cpf", pessoa.Cpf, System.Data.DbType.String);
+
+        var sql = @"UPDATE Pessoa
+                    SET
+                        LimiteCredito = @LimiteCredito,
+                    WHERE
+                        Cpf = @Cpf";
+
+        var retorno = await ExecuteQueryAsync<PessoaCreditoEntity>(sql, parametros);
+
+        return retorno.FirstOrDefault();
+    }
+}
+
---
+++ src/Cepedi.Banco.Analise.Dominio/Repositorio/Queries/IPessoaCreditoQueryRepository.cs
@@ -0,0 +1,7 @@
+﻿using Cepedi.Banco.Analise.Dominio.Entidades;
+
+namespace Cepedi.Banco.Analise.Dominio.Repositorio.Queries;
+public interface IPessoaCreditoQueryRepository
+{
+    Task<List<PessoaCreditoEntity>> ObterPessoasAsync(string cpf);
+}
